fbf7214271ab1efc9c5a087a66387e4272c2876f
Rajkrishnan <rkrishnan1993@live.com>
2014-09-19 22:23:18 +0530

    working file

--- src/main.java
+++ src/main.java
@@ -4,7 +4,7 @@ import java.awt.event.*;
 public class main extends Applet implements ActionListener {
         Button check;
         TextField height,weight,result;
-        Label l1,l2,l3;
+        Label l1,l2,l3,not;
         public void init()
         {
             setLayout(new GridLayout(4,4));
@@ -14,26 +14,51 @@ public class main extends Applet implements ActionListener {
             result = new TextField();
             l1 = new Label("Height in Centimeters ");
             l2 = new Label("Weight in Kilogram");
-            l3 = new Label("Result");
+            l3 = new Label("Index Value");
+            not=new Label("Status");
+            not.setBackground(Color.black);
+            Font notf = new Font("Arial",Font.BOLD,30);
+            not.setFont(notf);
             add(l1);
             add(height);
-                     add(l2);
-                     add(weight);
-                        add(l3);
-add(result);
-               add(check);
-
+            add(l2);
+            add(weight);
+            add(l3);
+            add(result);
+            add(check);
+            add(not);
             check.addActionListener(this);
         }
         public void actionPerformed(ActionEvent ae)
         {
             String s=ae.getActionCommand();
-            int w,h,r;
+            double we,h;
+            double r;
+            we=Integer.parseInt(weight.getText());
             h=Integer.parseInt(height.getText());
-            w=Integer.parseInt(weight.getText());
-            h=h/100;
-            r = (w/h);
+            h=(h/100);h=h*h;
+            r = we/h;
             result.setText(String.valueOf(r));
+            if(r<18.5)
+            {
+                not.setForeground(Color.YELLOW);
+                not.setText(String.valueOf("UnderWeight"));
+            }
+            if(r>=18.5&&r<24.9)
+            {
+                 not.setForeground(Color.GREEN);
+                not.setText(String.valueOf("Normal"));
+            }
+            if(r>25&&r<29.9)
+            {
+                not.setForeground(Color.ORANGE);
+                not.setText(String.valueOf("Overweight"));
+            }
+            if(r>=30)
+            {
+                not.setForeground(Color.RED);
+                not.setText(String.valueOf("Obese"));
+            }
             repaint();
         }
         public void paint(Graphics g)
